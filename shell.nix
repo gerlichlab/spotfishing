414df3922fea81e99ca2337c7c1758a0b6ed3e37
Vince Reuter <vince.reuter@gmail.com>
2024-03-14 10:46:51 +0100

    add dev dependencies hook for Nix shell; simplify

--- shell.nix
+++ shell.nix
@@ -2,14 +2,15 @@
   pkgs ? import (builtins.fetchGit {
     url = "https://github.com/NixOS/nixpkgs/";
     ref = "refs/tags/23.11";
-  }) {}
+  }) {}, 
+  dev ? true,
 }:
 let 
   py310 = pkgs.python310;
-  poetryExtras = [];
+  poetryExtras = if dev then ["dev"] else [];
   poetryInstallExtras = (
     if poetryExtras == [] then ""
-    else pkgs.lib.concatStrings [ " -E " (pkgs.lib.concatStringsSep " -E " poetryExtras) ]
+    else pkgs.lib.concatStrings [ " --with=" (pkgs.lib.concatStringsSep "," poetryExtras) ]
   );
 in
 pkgs.mkShell {
@@ -19,14 +20,13 @@ pkgs.mkShell {
     py310
     python311
     python312
-  ];
+  ] ++ (if dev then [ pkgs.poetryPlugins.poetry-plugin-export ] else []);
   shellHook = ''
     # To get this working on the lab machine, we need to modify Poetry's keyring interaction:
     # https://stackoverflow.com/questions/74438817/poetry-failed-to-unlock-the-collection
     # https://github.com/python-poetry/poetry/issues/1917
     export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
     poetry env use "${py310}/bin/python"
-    export LD_LIBRARY_PATH="${pkgs.zlib}/lib:${pkgs.stdenv.cc.cc.lib}/lib"
     poetry install -vv --sync${poetryInstallExtras}
     source "$(poetry env info --path)/bin/activate"
   '';
