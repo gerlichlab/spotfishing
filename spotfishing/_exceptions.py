4eeed3abcb8b1d242b756b883e313a7896f2b4ff
Vince Reuter <vince.reuter@gmail.com>
2024-03-19 03:04:19 +0100

    first-pass at full flexibility with prelim tests (accordance with previous settings) passing

--- spotfishing/_exceptions.py
+++ spotfishing/_exceptions.py
@@ -3,6 +3,10 @@
 __author__ = "Vince Reuter"
 __credits__ = ["Vince Reuter"]
 
+__all__ = [
+    "DimensionalityError",
+]
+
 
 class DimensionalityError(Exception):
     """Error subtype when dimensionality of something isn't as expected"""
--- spotfishing/_numeric_types.py
+++ spotfishing/_numeric_types.py
@@ -9,6 +9,6 @@ __credits__ = ["Vince Reuter"]
 
 __all__ = ["NumpyInt", "NumpyFloat", "PixelValue"]
 
-NumpyInt = Union[np.int8, np.int16, np.int32, np.int64]
 NumpyFloat = Union[np.float16, np.float32, np.float64]
+NumpyInt = Union[np.int8, np.int16, np.int32, np.int64]
 PixelValue = Union[np.int8, np.int16]
