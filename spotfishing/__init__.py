9cd3643777184f703e9089b66a71a8692921b42a
Vince Reuter <vince.reuter@gmail.com>
2024-03-20 03:23:09 +0100

    separate project for looptrace-related

--- spotfishing/__init__.py
+++ spotfishing/__init__.py
@@ -5,14 +5,12 @@ from ._exceptions import *
 from .detection_result import DetectionResult
 from .detectors import detect_spots_dog, detect_spots_int
 from .dog_transform import DifferenceOfGaussiansTransformation
-from spotfishing_looptrace import DifferenceOfGaussiansSpecificationForLooptrace
 
 __author__ = "Vince Reuter"
 __credits__ = ["Vince Reuter", "Kai Sandoval Beckwith"]
 
 __all__ = [
     "ROI_MEAN_INTENSITY_KEY",
-    "DifferenceOfGaussiansSpecificationForLooptrace",
     "DifferenceOfGaussiansTransformation",
     "DimensionalityError",
     "DetectionResult",
