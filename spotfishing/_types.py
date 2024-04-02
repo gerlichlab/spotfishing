08c1c3db200750446c994cfafe1f7781c3ad4662
Vince Reuter <vince.reuter@gmail.com>
2024-04-02 23:06:22 +0200

    fix definition of pixel value type to be unsigned (nonnegative)

--- spotfishing/_types.py
+++ spotfishing/_types.py
@@ -19,4 +19,4 @@ NumpyFloat = Union[np.float16, np.float32, np.float64]
 
 NumpyInt = Union[np.int8, np.int16, np.int32, np.int64]
 
-PixelValue = Union[np.int8, np.int16]
+PixelValue = Union[np.uint8, np.uint16]
